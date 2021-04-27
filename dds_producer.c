dc2a7b51f082e7903cd3884efb521332641824a6
Zhi Kai Lim <liquidxhendrix@gmail.com>
2021-04-27 09:12:26 +0800

    Start of Task 4 - DDS Producer

---
+++ dds_producer.c
@@ -0,0 +1,6 @@
+#include "dds_producer.h"
+
+int main(int argc, char** argv) {
+      printf("Hello, World! From Client\n");
+
+}
---
+++ dds_producer.h
@@ -0,0 +1,12 @@
+#ifndef DDS_PRODUCER
+#define DDS_PRODUCER
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <signal.h> 
+
+
+#endif
